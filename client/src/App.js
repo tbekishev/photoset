8fcd239d725ac09a932402536e9e022a68e61672
Tuigun Bekishev <tbekishev@gmail.com>
2023-01-29 23:32:31 -0400

    add Home and Login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello World.
-    </div>
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
   );
 }
 
