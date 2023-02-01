deb19d32ee94e5dd7cf505f0e9af67720edfa7c8
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-01 17:55:17 -0400

    add spinner loading

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spinner from './Spinner';
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) return <Spinner message='We are adding new ideas to your feed!' />
   return (
     <div>
       
---
+++ client/src/components/MasonryLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MasonryLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MasonryLayout
