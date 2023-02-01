89fabeba27592c96e789504e3488546343f0bff4
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-01 19:06:07 -0400

    fetch data and show

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams(null);
+  const [pins, setPins] = useState();
+
+  useEffect(() => {
+
+    setLoading(true);
+    if(categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    } else {
+      client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [categoryId])
 
   if (loading) return <Spinner message='We are adding new ideas to your feed!' />
+  
   return (
     <div>
-      
+      {pins && <MasonryLayout pins={pins} /> }
     </div>
   )
 }
