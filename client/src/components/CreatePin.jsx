ab53227c361850c3eab1a7e4dbc480d3466264dd
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-09 16:07:17 -0400

    add upload function

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -3,6 +3,7 @@ import { AiOutlineCloudUpload } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
 import { client } from '../client';
+import { categories } from '../utils/data';
 import Spinner from './Spinner';
 
 const CreatePin = ({ user }) => {
@@ -14,6 +15,9 @@ const CreatePin = ({ user }) => {
   const [wrongImageType, setWrongImageType] = useState(false);
   const [imageAsset, setImageAsset] = useState(false);
   const [title, setTitle] = useState();
+  const [about, setAbout] = useState();
+  const [destination, setDestination] = useState();
+  const [category, setCategory] =useState();
 
   const uploadImage = (e) => {
     const { type, name } = e.target.files[0];
@@ -33,6 +37,38 @@ const CreatePin = ({ user }) => {
     }
   }
 
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id,
+        },
+        category,
+      }
+      client
+        .create(doc)
+        .then(() => {
+          navigate('/');
+        })
+    } else {
+      setFields(true);
+      setTimeout(() => setFields(false), 2000);
+    }
+  }
+
   return (
     <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
       {fields && (
@@ -46,7 +82,7 @@ const CreatePin = ({ user }) => {
             {!imageAsset ? (
               <label>
                 <div className='flex flex-col items-center justify-center h-full'>
-                  <div className='flex flex-col justify-center items-center'>
+                  <div className='flex flex-col justify-center items-center cursor-pointer'>
                     <p className='font-bold text-2xl'>
                       <AiOutlineCloudUpload />
                     </p>
@@ -86,8 +122,61 @@ const CreatePin = ({ user }) => {
         <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full'>
           <input
             type='text'
-            value={title}          
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder='Add your title here'
+            className='outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2'
           />
+          {user && (
+            <div className='flex gap-2 my-2 items-center bg-white rounded-lg'>
+              <img
+                src={user.image}
+                className='w-10 h-10 rounded-full'
+                alt='user-profile'
+              />
+              <p className='font-bold'>{user.userName}</p>
+            </div>
+          )}
+          <input
+            type='text'
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder='What is your pin about?'
+            className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+          />
+          <input
+            type='text'
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder='Add a destination link'
+            className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+          />
+          <div className='flex flex-col'>
+            <div>
+              <p className='mb-2 font-semibold text-lg sm:text-xl'>Choose Pin Category</p>
+              <select
+                onChange={(e) => setCategory(e.target.value)}
+                className='outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'
+              >
+                <option value='other' className='bg-white'>Select Category</option>
+                {categories.map((category) => (
+                  <option 
+                    className='text-base border-0 outline-none capitalize bg-white text-black'
+                    value={category.name}
+                  >{category.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className='flex justify-end items-center mt-5'>
+              <button
+                type='button'
+                onClick={savePin}
+                className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none'
+              >
+                Save Pin
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
