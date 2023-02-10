f64b4c9f793ea0c6da73c36201556080829d7058
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-10 16:45:48 -0400

    add comment function

--- client/src/components/PinDetail.jsx
+++ client/src/components/PinDetail.jsx
@@ -4,11 +4,15 @@ import { Link, useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { client, urlFor } from '../client';
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const PinDetail = ({ user }) => {
+
   const [pinDetail, setPinDetail] = useState(null);
   const [pins, setPins] = useState();
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();  
   
   const fetchPinDetails = () => {
@@ -24,14 +28,39 @@ const PinDetail = ({ user }) => {
         .then((res) => setPins(res))
       }
     })
-  }
+  };
   
   useEffect(() => {
     fetchPinDetails();
-  }, [pinId])
+  }, [pinId]);
+
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert('after', 'comments[-1]', [{
+          comment, 
+          _key: uuidv4(), 
+          postedBy: { 
+            _type: 'postedBy', 
+            _ref: user._id 
+          } 
+        }])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment('');
+          setAddingComment(false);
+        });
+    }
+  };
 
   if(!pinDetail) return <Spinner message="Loading pin..." />
   return (
+    <>
     <div 
       className='flex xl-flex-row flex-col m-auto bg-white'
       style={{maxWidth: '1500px', borderRadius: '32px'}}
@@ -101,8 +130,44 @@ const PinDetail = ({ user }) => {
             </div>
           ))}
         </div>
+        <div className='flex flex-wrap mt-6 gap-3'>
+          <Link 
+            to={`user-profile/${pinDetail.postedBy?._id}`}
+          >
+            <img 
+              className='w-10 h-10 rounded-full cursor-pointer'
+              src={pinDetail.postedBy?.image}
+              alt='user-profile'
+            />
+          </Link>
+          <input 
+            className='flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300'
+            type='text'
+            placeholder='Add a comment'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            type='button'
+            className='bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none'
+            onClick={addComment}
+          >
+            {addingComment ? 'Posting the comment...' : 'Post'}
+          </button>
+        </div>
       </div>
     </div>
+    {pins?.length > 0 ? (
+      <>
+        <h2 className='text-center font-bold text-2x mt-8 mb-4'>
+          More like this
+        </h2>
+        <MasonryLayout pins={pins} />
+      </>
+    ) : (
+      <Spinner message='Loading more pins...' />
+    )}
+  </>
   )
 }
 
