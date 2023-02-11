601545b1d2d58e25704cc4fa1d9f3a92acb5f4e5
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-11 14:29:01 -0400

    add search function

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
-const Search = () => {
+const Search = ({ searchTerm }) => {
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState();
+
+  useEffect(() => {
+    if(searchTerm !== '') {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client
+        .fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    }
+    else {
+      client
+        .fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [searchTerm])
+
   return (
     <div>
-      Search
+      {loading && <Spinner message='Searching for pins...'/>}
+      {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className='mt-10 text-center text-xl'>
+          No Pins Found
+        </div>
+      )}
     </div>
   )
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,18 +2,11 @@ import React from 'react';
 import { RiHomeFill } from 'react-icons/ri';
 import logo from '../assets/logo.png';
 import { Link, NavLink } from 'react-router-dom';
+import { categories } from '../utils/data';
 
-const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
-const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
-const categories = [
-  { name: 'Animals' },
-  { name: 'Wallpapers' },
-  { name: 'Photography' },
-  { name: 'Gaming' },
-  { name: 'Coding' },
-  { name: 'Travel' },
-  { name: 'Other' },
-]
+const isNotActiveStyle = 'flex mb-3 items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex mb-3 items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+
 const Sidebar = ({user, closeToggle}) => {
   const handleCloseSidebar = () => {
     if(closeToggle) closeToggle(false);
@@ -37,14 +30,19 @@ const Sidebar = ({user, closeToggle}) => {
             <RiHomeFill />
             Home
           </NavLink>
-          <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Categories</h3>
-          {categories.slice(0, categories.length - 1).map((category) => (
+          <h3 className='mb-3 mt-2 px-5 text-base 2xl:text-xl'>Categories</h3>
+          {categories.map((category) => (
             <NavLink
               to={`/category/${category.name}`}
               className={({isActive}) => isActive ? isActiveStyle : isNotActiveStyle}
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className='w-8 h-8 rounded-full shadow-sm'
+                alt='category'
+              />
               {category.name}
             </NavLink>
           ))}
